f66ee06088feca5c23f14c8036360e3ec5c6fa83
Suprememajor <ndipnobertetta@gmail.com>
2022-11-15 03:29:17 +0100

    basic controls setup

--- src/component/Agenda.js
+++ src/component/Agenda.js
@@ -1,7 +1,117 @@
-import React from "react";
+import React, { Component } from "react";
+import moment from "moment";
 
-const Agenda = () => {
-  return <div>Agenda</div>;
-};
+// items import
+import { items } from "../utils/items";
+
+// colors import
+import { colors } from "../utils/colors";
+
+class Agenda extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [],
+      selected: [],
+      cellHeight: 60 / 4,
+      showModal: false,
+      locale: "fr",
+      rowsPerHour: 4,
+      numberOfDays: 4,
+      startDate: new Date(),
+    };
+    this.zoomIn = this.zoomIn.bind(this);
+    this.zoomOut = this.zoomOut.bind(this);
+    this._openModal = this._openModal.bind(this);
+    this._closeModal = this._closeModal.bind(this);
+    this.changeView = this.changeView.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ items: items });
+  }
+
+  componentDidUpdate(next, last) {
+    if (next.items) {
+      this.setState({ items: next.items });
+    }
+  }
+
+  zoomIn() {
+    var num = this.state.cellHeight + 15;
+    this.setState({ cellHeight: num });
+  }
+
+  zoomOut() {
+    var num = this.state.cellHeight - 15;
+    this.setState({ cellHeight: num });
+  }
+
+  _openModal() {
+    this.setState({ showModal: true });
+  }
+
+  _closeModal(e) {
+    if (e) {
+      e.stopPropagation();
+      e.preventDefault();
+    }
+    this.setState({ showModal: false });
+  }
+
+  changeView(days, event) {
+    this.setState({ numberOfDays: days });
+  }
+
+  render() {
+    return (
+      <div className="content-expanded">
+        <div className="control-buttons">
+          <button className="button-control" onClick={this.zoomIn}>
+            {" "}
+            <i className="zoom-plus-icon"></i>{" "}
+          </button>
+          <button className="button-control" onClick={this.zoomOut}>
+            {" "}
+            <i className="zoom-minus-icon"></i>{" "}
+          </button>
+          <button className="button-control" onClick={this._openModal}>
+            {" "}
+            <i className="schedule-icon"></i>{" "}
+          </button>
+          <button
+            className="button-control"
+            onClick={this.changeView.bind(null, 7)}
+          >
+            {" "}
+            {moment.duration(7, "days").humanize()}{" "}
+          </button>
+          <button
+            className="button-control"
+            onClick={this.changeView.bind(null, 4)}
+          >
+            {" "}
+            {moment.duration(4, "days").humanize()}{" "}
+          </button>
+          <button
+            className="button-control"
+            onClick={this.changeView.bind(null, 3)}
+          >
+            {" "}
+            {moment.duration(3, "days").humanize()}{" "}
+          </button>
+          <button
+            className="button-control"
+            onClick={this.changeView.bind(null, 1)}
+          >
+            {" "}
+            {moment.duration(1, "day").humanize()}{" "}
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Agenda;
